c962d9a2e64889abeab8c311cdb5ae62883ce776
thomasrosen <thomas.rosen@me.com>
2022-04-03 00:37:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const { loadModule } = require('cld3-asm')
 
 const findLanguage = {
   language: 'ko',
-  text:
-    ' 개별적으로 리포트 액세스 권한을 부여할 수 있습니다 액세스 권한 부여사용자에게 프로필 리포트에 액세스할 수 있는 권한을 부여하시려면 가용 프로필 상자에서 프로필 이름을 선택한 다음'
+  text: ' 개별적으로 리포트 액세스 권한을 부여할 수 있습니다 액세스 권한 부여사용자에게 프로필 리포트에 액세스할 수 있는 권한을 부여하시려면 가용 프로필 상자에서 프로필 이름을 선택한 다음'
 }
 
 const frequentLanguage = 'This piece of text is in English. Този текст е на Български.'
@@ -14,13 +13,10 @@ async function start() {
   const identifier = cldFactory.create(0, 1000)
 
   const findResult = identifier.findLanguage(findLanguage.text)
-  console.log(`detect languages for text "${findLanguage.text.substring(0, 25)}..."`)
-  console.log(JSON.stringify(findResult))
-  console.log(findResult.language === findLanguage.language)
+  console.log('locale:', JSON.stringify(findResult))
 
   const frequentResult = identifier.findMostFrequentLanguages(frequentLanguage, 3)
-  console.log(`find most frequent languages for text "${frequentLanguage}..."`)
-  console.log(JSON.stringify(frequentResult))
+  console.log('locales:', JSON.stringify(frequentResult))
 
   identifier.dispose()
 }
